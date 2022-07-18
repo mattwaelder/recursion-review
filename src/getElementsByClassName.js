b59c5594e07ba8b37e54057c93358bd617155b08
Matt Waelder <mattwaelder@gmail.com>
2022-07-18 15:28:20 -0700

    initial commit

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,43 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+
+  var result = [];
+
+
+  var getsMatchingNodes = function (node) {
+    //base case
+    //check if classname
+    if (node.classList.contains(className)) {
+      result.push(node);
+    }
+    if (node.children.length === 0) {
+      return;
+    }
+
+    //if passed in node has children
+    //recursive case
+    if (node.children.length > 0) {
+      //itterate over each child recursively calling helper fn
+      for (var x = 0; x < node.children.length; x++) {
+        var currentNode = node.children[x];
+        getsMatchingNodes(currentNode);
+      }
+    }
+  };
+
+
+  //input is a string (maybe an array/collection, too)
+  //output is array
+  //if none of the nodes match, return empty arrray
+
+  //if current node matches, add to result
+  //itterate through current node
+  //if it has children, itterate over its children (recursive)
+
+  getsMatchingNodes(document.body);
+
+
+  console.log(result);
+  return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,63 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  console.log('passed in: ', obj);
+  var result = '';
+
+  if (obj === null) {
+    return 'null';
+  }
+  /*if (obj.length === 0 || obj === undefined) {
+    return '';
+  }*/
+  if (typeof obj === 'boolean') {
+    return String(obj);
+  }
+  if (typeof obj === 'number') {
+    return String(obj);
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+
+
+  if (typeof obj === 'object') {
+    if (Array.isArray(obj)) {
+      //treat like an true array
+      if (obj.length === 0) {
+        return '[]';
+      } else {
+        //recursive case for arrays
+        result += '[';
+        for (var i = 0; i < obj.length; i++) {
+          var lastIndex = obj.length - 1;
+          //if its not the last item in the array
+          if (i !== lastIndex) {
+            result += stringifyJSON(obj[i]) + ',';
+          } else {
+            //if it is the last item in the array
+            result += stringifyJSON(obj[i]);
+          }
+        }
+        result += ']';
+      }
+
+      //for objects (not arrays)
+    } else {
+      if (Object.keys(obj).length === 0) {
+        return '{}';
+      }
+      //treat like an obj
+      // var lastKey = Object.keys(obj).pop();
+      // console.log(lastKey)
+    }
+  }
+
+
+
+  //base case: if obj does not have nested content
+  //recurssive: if obj has nested content, itterate
+  console.log('result: ', result);
+  return result;
 };
